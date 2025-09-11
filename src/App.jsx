4a1139122847a3591a8e011f045611e7b10df3fa
RumRage <claravillarruel4@gmail.com>
2025-09-11 13:24:44 -0300

    ADD pages & routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Listing from "./pages/Listing";
 
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
