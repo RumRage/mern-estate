99f95c6443225ee8bb7d4d08ff8abec91cff10c0
RumRage <claravillarruel4@gmail.com>
2025-09-11 16:35:03 -0300

    ADD google & logout

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-e1ff9.firebaseapp.com",
+  projectId: "mern-estate-e1ff9",
+  storageBucket: "mern-estate-e1ff9.firebasestorage.app",
+  messagingSenderId: "1093004950552",
+  appId: "1:1093004950552:web:8a4279f1ec7d72da5a976d",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
