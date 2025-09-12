d1c3102a0d864be01275cc202bd5ef2b154f62dd
RumRage <claravillarruel4@gmail.com>
2025-09-11 21:08:45 -0300

    ADD update user & delete account

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,11 +4,12 @@ import {
   test,
   updateUser,
 } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-router.post("/update/:id", updateUser);
-router.delete("/delete/:id", deleteUser);
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
