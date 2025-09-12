39336d346a89c51ba8c27fff2786254c7baad698
RumRage <claravillarruel4@gmail.com>
2025-09-11 21:42:17 -0300

    FIX

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -8,12 +8,12 @@ import {
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
+const router = express.Router();
+
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
-router.get("/get/:id", verifyToken, getListing);
-router.get("/get", verifyToken, getListings);
-
-const router = express.Router();
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
